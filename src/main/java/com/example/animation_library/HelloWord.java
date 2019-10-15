204786525b9777ee188535b8cd03381d4762b54e
pinghu_1 <hu_456257ping>
2019-10-15 17:10:21 +0800

    Initial commit

---
+++ src/main/java/com/example/animation_library/HelloWord.java
@@ -0,0 +1,11 @@
+package com.example.animation_library;
+
+import android.util.Log;
+
+import static android.content.ContentValues.TAG;
+
+public class HelloWord {
+    public static void hello() {
+        Log.d(TAG, "hello: 这是我的第一个库");
+    }
+}
